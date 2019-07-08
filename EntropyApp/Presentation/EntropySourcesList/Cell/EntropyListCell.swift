6079e26a791c98daaf6fbe645cbbceca969f6df3
Андрей Зорькин <zooorkin@icloud.com>
2019-07-08 21:11:02 +0300

    Добавлен список источников энтропии и экран источника энтропии.

---
+++ EntropyApp/Presentation/EntropySourcesList/Cell/EntropyListCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntropyListCell.swift
+//  Entropy
+//
+//  Created by Андрей Зорькин on 01/07/2019.
+//  Copyright © 2019 Андрей Зорькин. All rights reserved.
+//
+
+import UIKit
+
+class EntropyListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
